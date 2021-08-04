a5994e90e40fb1087417bb8779dfc814dd263db2
Matt <mtaylor@nuroai>
2021-08-04 13:28:54 -0700

    data acquisition updates

--- fan/fan_main.py
+++ fan/fan_main.py
@@ -140,7 +140,7 @@ def main(MODE, RUN_TIME, DUTY, REP):
             file_raw_row.append(RUN_TIME)                # duration
             file_raw_row.append(DUTY)                # PWM
             file_raw_row.append(RPM)                # Avg RPM
-            writer.writerow(file_main_row)
+            writer.writerow(file_raw_row)
         except KeyboardInterrupt:
             print("*****************************")
             print("\nTest Cancelled\n")
@@ -233,8 +233,8 @@ if __name__ == "__main__":
                     file_main_row.append(FILE_OUTPUT_NAME)           # timestamp
                     file_main_row.append(i + 1)                      # mode number
                     file_main_row.append(j + 1)                      # repetition number
-                    file_main_row.append(settings[0])                # duration
-                    file_main_row.append(settings[1])                # PWM
+                    file_main_row.append(settings[0][i])                # duration
+                    file_main_row.append(settings[1][i])                # PWM
                     file_main_row.append(RPM_AVG[-1])                # Avg RPM
                     writer.writerow(file_main_row)
             display_results(RPM_AVG, settings)
